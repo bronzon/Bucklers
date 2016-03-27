df95422881a5fab52b99f5f835cbf65e8110470e
bronzon <olle.back@gmail.com>
2016-03-27 23:01:15 +0200

    Controlling size of player using a per scene configurable prefab

---
+++ TheBucklers/Assets/scripts/CharacterSizeController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterSizeController : MonoBehaviour {
+	public float maxAtUnits = -5;
+	public float minAtUnits = 5;
+	public float maxScale = 5;
+	public float minScale = 1;
+
+	public GameObject character;
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float unitDiff = Mathf.Abs(maxAtUnits - minAtUnits);
+		float playerY = character.transform.position.y;
+		float distanceTravelledFromMin = Mathf.Abs(Mathf.Clamp(playerY, maxAtUnits, minAtUnits) - minAtUnits);
+		float scalePercentage = distanceTravelledFromMin / unitDiff;
+		float scaleDiff = maxScale - minScale;
+		float scaledOffset = scaleDiff * scalePercentage;
+		float calculatedScale = scaledOffset + minScale;
+		character.transform.localScale = new Vector3 (calculatedScale, calculatedScale);
+	}
+}
---
+++ TheBucklers/Assets/scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	public float speed = 1;
+	private Animator animator;
+
+
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Vector2 direction = new Vector2(Input.GetAxisRaw ("Horizontal"),Input.GetAxisRaw ("Vertical"));
+		if (direction.magnitude == 0) {
+			animator.enabled = false;
+		} else {
+			animator.enabled = true;
+		}
+		animator.SetInteger ("horizontal", (int)direction.x);
+		animator.SetInteger ("vertical", (int)direction.y);
+
+		if (direction.x > 0) {
+			transform.localRotation = Quaternion.Euler (0, 0, 0);
+		} else {
+			transform.localRotation = Quaternion.Euler(0, 180, 0);
+		}
+
+		if (direction.magnitude > float.Epsilon) {
+			Vector2 estimatedPos = new Vector2(transform.position.x + direction.x*speed, transform.position.y + direction.y*speed);
+			RaycastHit2D hit = Physics2D.Linecast (transform.position, estimatedPos);
+			if (hit.collider == null) {
+				transform.position = estimatedPos;
+			}
+		}
+	}
+}
