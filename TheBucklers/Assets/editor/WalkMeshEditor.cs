6aa39658a809e144cbe1bce39fb76c7d249d8c32
bronzon <olle.back@gmail.com>
2016-03-31 23:13:24 +0200

    started working on custom walkmesh

---
+++ TheBucklers/Assets/editor/WalkMeshEditor.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using UnityEditor;
+
+[CustomEditor(typeof(WalkMesh))]
+class WalkMeshEditor : Editor {
+	private WalkMesh walkMesh;
+
+	void OnEnable() {
+		walkMesh = target as WalkMesh;	
+	}
+
+    void OnSceneGUI () {
+		if(walkMesh.points.Count >= 2) {
+			walkMesh.points.Add (walkMesh.points [0]); //close the loop before drawing
+			Handles.DrawPolyLine(walkMesh.points.ToArray());
+			walkMesh.points.RemoveAt (walkMesh.points.Count - 1);
+		}   
+
+		if (Event.current.type == EventType.KeyDown && Event.current.keyCode == KeyCode.Space) {
+			Vector2 mousePos = Event.current.mousePosition;
+			mousePos.y = Camera.current.pixelHeight - mousePos.y;
+			Vector3 position = Camera.current.ScreenPointToRay(mousePos).origin;
+			position.z = 0;
+			walkMesh.points.Add (position);
+		}
+	}
+}
+
---
+++ TheBucklers/Assets/scripts/WalkMesh.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class WalkMesh : MonoBehaviour {
+	public List<Vector3> points = new List<Vector3> ();
+
+	void Start() {
+		
+	}
+
+	public bool isInside(Vector2 pos) {
+		return false;
+	}
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
