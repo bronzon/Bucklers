48c276aef157449dfb55fe6079e61fb80bbadcfd
pontus-eriksson <pontus.eriksson@king.com>
2016-04-14 22:09:44 +0200

    added Swamp scene and ability to move to bay

---
+++ TheBucklers/Assets/scripts/SceneSwitcher.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using UnityEngine.Assertions;
+
+public class SceneSwitcher : MonoBehaviour {
+	public string sceneName;
+	public GameObject player;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (GetComponent<PolygonCollider2D>().OverlapPoint(player.GetComponent<Transform>().position)) {
+			SceneManager.LoadScene(sceneName);
+			Scene scene = SceneManager.GetSceneByName(sceneName);
+			Assert.IsTrue(scene.IsValid());
+			SceneManager.SetActiveScene(scene);
+		}
+
+
+	}
+}
